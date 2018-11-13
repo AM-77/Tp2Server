e70260ad92ee0b9407b15dee151dea4af3410dc1
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-13 18:10:48 +0100

    Added the State class.

--- src/tp2Server/State.java
+++ src/tp2Server/State.java
@@ -1,29 +1,21 @@
 package tp2Server;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class State {
-	private static final long serialVersionUID = 1L;
+public class State implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
 	private String id;
 	private int q;
 	private Date lastOp;
 	
-	public State(String id){
+	public State(String id, int Q, Date lastOp){
 		this.id = id;
-		
-		this.q = this.get_q();
-		this.lastOp = this.get_lastOp();
+		this.q = Q;
+		this.lastOp = lastOp;
 	}
 	
-	public int get_q(){
-		return 0;
-	}
-	
-	public Date get_lastOp(){
-		return new Date();
-	}
-
 	public String getId() {
 		return id;
 	}
