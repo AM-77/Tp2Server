43a628859f3530f2a7985b174bb39d3ad1a4a6e9
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-15 18:32:28 +0100

    Updated StockImpl

---
+++ src/tp2/StockImpl.java
@@ -0,0 +1,44 @@
+package tp2;
+
+import java.rmi.RemoteException;
+
+import tp2.DetabaseConnection;
+
+public class StockImpl implements IStock {
+
+	private DetabaseConnection dbConnect = new DetabaseConnection();
+	
+	protected StockImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public void createArticle(String id, int Q_init) {
+		dbConnect.addArticle("INSERT INTO `Articles` (`id`, `quantity`, `lastOp`) VALUES ('"+ id +"', '"+ Q_init +"', CURRENT_TIMESTAMP)");
+	}
+
+	@Override
+	public boolean sale(String id, int Q) {
+		int quantity = dbConnect.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
+		if(Q <= quantity){
+			dbConnect.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity - Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void provision(String id, int Q) {
+		int quantity = dbConnect.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
+		dbConnect.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity + Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
+	}
+
+	@Override
+	public State state(String id) {
+		Article article = dbConnect.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'");
+		State s = new State(article.getId(), article.getQ(), article.getLastOp());
+		return s;
+	}
+
+}
