30a8e5e3f5734ed823fe4a8ac44913c63d6a7b4a
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-16 15:40:02 +0100

    Updated StockImpl class.

--- src/tp2/StockImpl.java
+++ src/tp2/StockImpl.java
@@ -13,29 +13,31 @@ public class StockImpl implements IStock {
 	}
 
 	@Override
-	public void createArticle(String id, int Q_init) {
+	public boolean createArticle(String id, int Q_init) throws Exception {
+		
 		dbConnect.addArticle("INSERT INTO `Articles` (`id`, `quantity`, `lastOp`) VALUES ('"+ id +"', '"+ Q_init +"', CURRENT_TIMESTAMP)");
+		return true;
 	}
 
 	@Override
-	public boolean sale(String id, int Q) {
+	public boolean sale(String id, int Q) throws Exception{
 		int quantity = dbConnect.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
 		if(Q <= quantity){
 			dbConnect.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity - Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
 			return true;
 		}
-		
 		return false;
 	}
 
 	@Override
-	public void provision(String id, int Q) {
+	public boolean provision(String id, int Q) throws Exception{
 		int quantity = dbConnect.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
 		dbConnect.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity + Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
+		return true;
 	}
 
 	@Override
-	public State state(String id) {
+	public State state(String id) throws Exception{
 		Article article = dbConnect.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'");
 		State s = new State(article.getId(), article.getQ(), article.getLastOp());
 		return s;
