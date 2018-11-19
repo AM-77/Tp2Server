2fe39b58791d7fff057c24e216c8c09efdf2b0bc
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-19 17:18:01 +0100

    Modified return values.

--- src/tp2/IStock.java
+++ src/tp2/IStock.java
@@ -1,16 +1,15 @@
 package tp2;
 
 import java.rmi.Remote;
-//import java.rmi.RemoteException;
 
 public interface IStock extends Remote{
 		
-	boolean createArticle(String id, int q_init) throws /* RemoteException, */ Exception;
+	boolean createArticle(String id, int q_init) throws Exception;
 	
-	boolean sale(String id, int q) throws /* RemoteException, */ Exception;
+	boolean sale(String id, int q) throws Exception;
 	
-	boolean provision(String id, int q) throws /* RemoteException, */ Exception;
+	boolean provision(String id, int q) throws Exception;
 	
-	State state(String id) throws /* RemoteException, */ Exception;
+	State state(String id) throws Exception;
 	
 }
