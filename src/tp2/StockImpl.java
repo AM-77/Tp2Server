83f163227e724a6bc3342faf5c82b07800c30ce4
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-19 17:16:17 +0100

    Updated StockImpl class.

--- src/tp2/StockImpl.java
+++ src/tp2/StockImpl.java
@@ -13,15 +13,15 @@ public class StockImpl implements IStock {
 	@Override
 	public boolean createArticle(String id, int Q_init) throws Exception {
 		
-		DetabaseConnection.addArticle("INSERT INTO `Articles` (`id`, `quantity`, `lastOp`) VALUES ('"+ id +"', '"+ Q_init +"', CURRENT_TIMESTAMP)");
+		DatabaseConnection.addArticle("INSERT INTO `Articles` (`id`, `quantity`, `lastOp`) VALUES ('"+ id +"', '"+ Q_init +"', CURRENT_TIMESTAMP)");
 		return true;
 	}
 
 	@Override
 	public boolean sale(String id, int Q) throws Exception{
-		int quantity = DetabaseConnection.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
+		int quantity = DatabaseConnection.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
 		if(Q <= quantity){
-			DetabaseConnection.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity - Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
+			DatabaseConnection.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity - Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
 			return true;
 		}
 		return false;
@@ -29,14 +29,14 @@ public class StockImpl implements IStock {
 
 	@Override
 	public boolean provision(String id, int Q) throws Exception{
-		int quantity = DetabaseConnection.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
-		DetabaseConnection.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity + Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
+		int quantity = DatabaseConnection.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'").getQ();
+		DatabaseConnection.addArticle("UPDATE `Articles` SET `quantity` = '" + ( quantity + Q ) + "', `lastOp` = CURRENT_TIMESTAMP  WHERE `Articles`.`id` = '"+ id +"'");			
 		return true;
 	}
 
 	@Override
 	public State state(String id) throws Exception{
-		Article article = DetabaseConnection.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'");
+		Article article = DatabaseConnection.getArticle("SELECT * FROM `Articles` WHERE id='"+id+"'");
 		State s = new State(article.getId(), article.getQ(), article.getLastOp());
 		return s;
 	}
